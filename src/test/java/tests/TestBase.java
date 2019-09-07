b049f8e7cc5c0a20bc789038b7fc1b1ef70bc73c
AyaHamdyAhmed <46898899+AyaHamdyAhmed@users.noreply.github.com>
2019-09-07 11:03:57 +0200

    Signed-off-by: AyaHamdyAhmed <46898899+AyaHamdyAhmed@users.noreply.github.com>

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,19 @@
+package tests;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterTest;
+
+public class TestBase {
+  @Test
+  public void f() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
