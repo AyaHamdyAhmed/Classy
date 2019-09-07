c3440f5d2482c7b4e7d5293a0bf57b61857e13da
AyaHamdyAhmed <46898899+AyaHamdyAhmed@users.noreply.github.com>
2019-09-07 17:52:46 +0200

    Add testBase

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,19 +1,28 @@
 package tests;
 
 import org.testng.annotations.Test;
+
+import Config.DriverManager;
+import Config.DriverManagerFactory;
+import Config.DriverType;
+
 import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 
 public class TestBase {
-  @Test
-  public void f() {
-  }
+	DriverManager driverManager;
+	WebDriver driver;
   @BeforeClass
   public void beforeClass() {
+	  driverManager = DriverManagerFactory.getDriverManager(DriverType.CHROME);
   }
 
   @AfterTest
   public void afterTest() {
+	  driver= driverManager.getWebDriver();
+	  driver.quit();
+	  
   }
 
 }
--- src/test/java/tests/Tests.java
+++ src/test/java/tests/Tests.java
@@ -4,16 +4,11 @@ import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import Config.DriverManager;
-import Config.DriverManagerFactory;
-import Config.DriverType;
-
-public class Tests {
-	DriverManager driverManager;
+public class Tests extends TestBase{
+	
 	WebDriver driver;
   @Test
   public void navigateToGoogleTest() {
-	  driverManager = DriverManagerFactory.getDriverManager(DriverType.CHROME);
 	  driver= driverManager.getWebDriver();
 	  driver.get("https://www.google.com");
 	  Assert.assertEquals(driver.getTitle(), "Google");
