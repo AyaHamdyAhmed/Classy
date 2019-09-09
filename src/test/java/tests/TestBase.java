a27712b3ce39d61f5b6f9b76efb1b46cdf67e78e
AyaHamdyAhmed <46898899+AyaHamdyAhmed@users.noreply.github.com>
2019-09-09 16:55:45 +0200

    EditMethodName

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,25 +1,25 @@
 package tests;
 
-import org.testng.annotations.Test;
-
 import Config.DriverManager;
 import Config.DriverManagerFactory;
 import Config.DriverType;
+import objectRepo.RespositoryParser;
 
 import org.testng.annotations.BeforeClass;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterTest;
+import org.testng.annotations.AfterClass;
 
 public class TestBase {
 	DriverManager driverManager;
 	WebDriver driver;
+	protected RespositoryParser parser;
   @BeforeClass
-  public void beforeClass() {
+  public void setup() {
 	  driverManager = DriverManagerFactory.getDriverManager(DriverType.CHROME);
   }
 
-  @AfterTest
-  public void afterTest() {
+  @AfterClass
+  public void tearDown() {
 	  driver= driverManager.getWebDriver();
 	  driver.quit();
 	  
