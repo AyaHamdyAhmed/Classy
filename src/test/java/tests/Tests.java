519bbc9dbfea7d91eac6d8f6c1f53978698eb9ff
AyaHamdyAhmed <46898899+AyaHamdyAhmed@users.noreply.github.com>
2019-09-09 17:13:36 +0200

    Create WebElements

--- src/test/java/tests/Tests.java
+++ src/test/java/tests/Tests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -8,9 +9,13 @@ public class Tests extends TestBase{
 	
 	WebDriver driver;
   @Test
-  public void navigateToGoogleTest() {
+  public void LoginTest() {
 	  driver= driverManager.getWebDriver();
-	  driver.get("https://www.google.com");
-	  Assert.assertEquals(driver.getTitle(), "Google");
+	  driver.get("https://login.classy.org/");
+	  WebElement UserName = driver.findElement(parser.getbjectLocator("UserName"));
+	  WebElement Password = driver.findElement(parser.getbjectLocator("Password"));
+	  UserName.sendKeys("dhalawa@classy.org");
+	  Password.sendKeys("12345678@A");
+	 // Assert.assertEquals(driver.getTitle(), "Google");
   }
 }
