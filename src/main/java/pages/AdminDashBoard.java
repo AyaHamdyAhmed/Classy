f886d1ee8e1df00fba2eefa03eb368a99e052bda
AyaHamdyAhmed <46898899+AyaHamdyAhmed@users.noreply.github.com>
2019-09-16 17:48:23 +0200

    commit

--- src/main/java/pages/AdminDashBoard.java
+++ src/main/java/pages/AdminDashBoard.java
@@ -1,11 +1,7 @@
 package pages;
 
-import java.io.IOException;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
-import objectRepo.RespositoryParser;
 
 public class AdminDashBoard extends PageBase {
 	public AdminDashBoard(WebDriver driver) {
@@ -14,19 +10,14 @@ public class AdminDashBoard extends PageBase {
 	}
 
 	public void OpenCampaignDashBoard() {
-		try {
-			parser = new RespositoryParser("application.properties");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		Initialize();
 		ClickOnCampaignsTab();
 	}
 
 	private void ClickOnCampaignsTab() {
 		implicitWaitUntilLoad(driver);
-		WebElement elem= driver.findElement(parser.getbjectLocator("CampaignTab"));
+		WebElement elem = driver.findElement(parser.getbjectLocator("CampaignTab"));
 		clickOnElementByJavaScript(driver, elem);
-		//ClickOnElement(driver, parser.getbjectLocator("CampaignTab"));
 		implicitWaitUntilLoad(driver);
 	}
 }
