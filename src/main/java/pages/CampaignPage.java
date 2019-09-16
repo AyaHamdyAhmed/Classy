a6548f4cb5ada56b053a5f7e273692a571ba984e
AyaHamdyAhmed <46898899+AyaHamdyAhmed@users.noreply.github.com>
2019-09-16 17:49:30 +0200

    Update CampaignPage.java
    optimize code

--- src/main/java/pages/CampaignPage.java
+++ src/main/java/pages/CampaignPage.java
@@ -1,5 +1,31 @@
 package pages;
 
-public class CampaignPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+public class CampaignPage extends PageBase {
+	public CampaignPage(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	public void createNewCampaign() {
+		Initialize();
+		ClickCreateNewButton();
+		ClickOnDonationItem();
+	}
+
+	private void ClickCreateNewButton() {
+		implicitWaitUntilLoad(driver);
+		WebElement elem = driver.findElement(parser.getbjectLocator("CreateNew"));
+		clickOnElementByJavaScript(driver, elem);
+		// ClickOnElement(driver, parser.getbjectLocator("CreateNew"));
+	}
+
+	private void ClickOnDonationItem() {
+		implicitWaitUntilLoad(driver);
+		WebElement elem = driver.findElement(parser.getbjectLocator("DonationListItem"));
+		clickOnElementByJavaScript(driver, elem);
+		//ClickOnElement(driver, parser.getbjectLocator("DonationListItem"));
+		implicitWaitUntilLoadWithLongWait(driver);
+	}
 }
