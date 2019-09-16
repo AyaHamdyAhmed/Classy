ae94d2982cfdad4ae2fed8ac94ec20e6dd01cf74
AyaHamdyAhmed <46898899+AyaHamdyAhmed@users.noreply.github.com>
2019-09-16 14:01:25 +0200

    Update AdminDashBoard.java
    use click with JS instead of selenium click

--- src/main/java/pages/AdminDashBoard.java
+++ src/main/java/pages/AdminDashBoard.java
@@ -1,19 +1,32 @@
 package pages;
 
-import org.openqa.selenium.WebDriver;
+import java.io.IOException;
 
-public class AdminDashBoard extends PageBase{
-     public AdminDashBoard(WebDriver driver) {
-    	 this.driver=driver;
-    	 
-     }
-     
-     public void OpenCampaignDashBoard() {
-    	 ClickOnCampaignsTab();
-     }
-     
-     private void ClickOnCampaignsTab() {
-    	 ClickOnElement(driver, parser.getbjectLocator("CampaignTab"));
-    	implicitWaitUntilLoad(driver);
-     }
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import objectRepo.RespositoryParser;
+
+public class AdminDashBoard extends PageBase {
+	public AdminDashBoard(WebDriver driver) {
+		this.driver = driver;
+
+	}
+
+	public void OpenCampaignDashBoard() {
+		try {
+			parser = new RespositoryParser("application.properties");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		ClickOnCampaignsTab();
+	}
+
+	private void ClickOnCampaignsTab() {
+		implicitWaitUntilLoad(driver);
+		WebElement elem= driver.findElement(parser.getbjectLocator("CampaignTab"));
+		clickOnElementByJavaScript(driver, elem);
+		//ClickOnElement(driver, parser.getbjectLocator("CampaignTab"));
+		implicitWaitUntilLoad(driver);
+	}
 }
