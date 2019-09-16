65655e348b30df84926baf2a1fdf0dc07f048ece
AyaHamdyAhmed <46898899+AyaHamdyAhmed@users.noreply.github.com>
2019-09-16 13:59:44 +0200

    Update Tests.java
    add new test step

--- src/test/java/tests/Tests.java
+++ src/test/java/tests/Tests.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeMethod;
 
+import pages.AdminDashBoard;
 import pages.HomePage;
 import pages.LoginPage;
 
@@ -12,18 +13,21 @@ public class Tests extends TestBase{
 	WebDriver driver;
 	HomePage home;
 	LoginPage login;
+	AdminDashBoard admin;
 	String url= "https://www.classy.org";
 	@BeforeMethod
 	public void navigation() {
 		driver= driverManager.getWebDriver();
 		home= new HomePage(driver);
 		login= new LoginPage(driver);
+		admin= new AdminDashBoard(driver);
 		home.goToUrl(url);
 	}
   @Test
   public void CreateNewDonationPageTest() {
 	  home.openLoginPage();
 	  login.Login("dhalawa@classy.org", "12345678@A");
+	  admin.OpenCampaignDashBoard();
 	  
   }
 }
