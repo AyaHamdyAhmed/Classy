b165ddc155f8087a1be4b9450f8afd7e63711f72
AyaHamdyAhmed <46898899+AyaHamdyAhmed@users.noreply.github.com>
2019-09-17 11:52:38 +0200

    Create CampaignOverviewPage.java

---
+++ src/main/java/pages/CampaignOverviewPage.java
@@ -0,0 +1,9 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class CampaignOverviewPage extends PageBase {
+	public CampaignOverviewPage(WebDriver driver) {
+		this.driver = driver;
+	}
+}
