5e9f2874de1467fb96577f60032087d719ba8c82
AyaHamdyAhmed <46898899+AyaHamdyAhmed@users.noreply.github.com>
2019-09-17 12:48:34 +0200

    Update CampaignOverviewPage.java
    add actions

--- src/main/java/pages/CampaignOverviewPage.java
+++ src/main/java/pages/CampaignOverviewPage.java
@@ -1,9 +1,28 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class CampaignOverviewPage extends PageBase {
+	WebElement elem;
+
 	public CampaignOverviewPage(WebDriver driver) {
 		this.driver = driver;
+	}
+
+	public void publishCampaign() {
+		Initialize();
+		clickPublishCamapaign();
+		clickPublishFromPopup();
+	}
+
+	private void clickPublishCamapaign() {
+		ClickOnElement(driver, parser.getbjectLocator("PublishBtn"));
+		implicitWaitUntilLoad(driver);
+	}
+
+	private void clickPublishFromPopup() {
+     elem= driver.findElement(parser.getbjectLocator("PublishPopup"));
+     clickOnElementByJavaScript(driver, elem);
 	}
 }
