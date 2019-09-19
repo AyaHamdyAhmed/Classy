a06cd6d8a69a73c4577fbfbbd3daf7888d183e68
AyaHamdyAhmed <46898899+AyaHamdyAhmed@users.noreply.github.com>
2019-09-19 17:31:12 +0200

    Update DeleteCampaign.java

--- src/main/java/pages/DeleteCampaign.java
+++ src/main/java/pages/DeleteCampaign.java
@@ -1,5 +1,40 @@
 package pages;
 
-public class DeleteCampaign extends PageBase{
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class DeleteCampaign extends PageBase {
+	WebElement elem;
+	public DeleteCampaign(WebDriver driver) {
+		this.driver = driver;
+
+	}
+	public void clearCreatedCampaigns(String CName) {
+		Initialize();
+		searchByCampaignName(CName);
+		ClicktoOpenCampainDetailsPage();
+		SwitchToNewTab(driver);
+		SwitchToCamapaignDetailsTab();
+	}
+	
+	
+	private void searchByCampaignName(String Cname) {
+		waitForElementToBeInteractable(driver, parser.getbjectLocator("CampaignSearchBox"));		
+		SendkeysToElemnent(driver, parser.getbjectLocator("CampaignSearchBox"), Cname);
+	}
+	
+	private void ClicktoOpenCampainDetailsPage() {
+		//// need to get search result element locator
+		elem = driver.findElement(parser.getbjectLocator(""));
+		clickOnElementByJavaScript(driver, elem);
+	}
+	
+    private void SwitchToCamapaignDetailsTab() {
+    	waitForElementToAppear(driver, parser.getbjectLocator("DetailsTab"));
+    	elem = driver.findElement(parser.getbjectLocator("DetailsTab"));
+    	clickOnElementByJavaScript(driver, elem);
+    }
+    
+    
 
 }
