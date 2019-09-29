a0f955ae25600c9ac95a4b9ab344fe6afeaa65c3
AyaHamdyAhmed <46898899+AyaHamdyAhmed@users.noreply.github.com>
2019-09-29 17:18:30 +0200

    Update AdminDashBoard.java

--- src/main/java/pages/AdminDashBoard.java
+++ src/main/java/pages/AdminDashBoard.java
@@ -1,11 +1,15 @@
 package pages;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class AdminDashBoard extends PageBase {
+	private Logger logger = Logger.getLogger(AdminDashBoard.class);
+
 	public AdminDashBoard(WebDriver driver) {
 		this.driver = driver;
+		LogConfig();
 
 	}
 
@@ -18,6 +22,8 @@ public class AdminDashBoard extends PageBase {
 		implicitWaitUntilLoad(driver);
 		WebElement elem = driver.findElement(parser.getbjectLocator("CampaignTab"));
 		clickOnElementByJavaScript(driver, elem);
+		logger.info("campaign tab is clicked");
 		implicitWaitUntilLoad(driver);
+		logger.info("campaign page is openned");
 	}
 }
