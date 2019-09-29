61347a389c812876b6c29c66535dec763e300d39
AyaHamdyAhmed <46898899+AyaHamdyAhmed@users.noreply.github.com>
2019-09-29 17:18:46 +0200

    Update HomePage.java

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,16 +1,20 @@
 package pages;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage extends PageBase {
 
+private Logger logger = Logger.getLogger(HomePage.class);
 	public HomePage(WebDriver driver) {
 		this.driver = driver;
+		LogConfig();
 	}
 	// keyWords
 
 	public void goToUrl(String url) {
 		driver.get(url);
+		logger.info("URL redirected");
 	}
 
 	public void openLoginPage() {
@@ -21,6 +25,7 @@ public class HomePage extends PageBase {
 	private void findHomeLoginBtn() {
 		ClickOnElement(driver, parser.getbjectLocator("Login"));
 		implicitWaitUntilLoad(driver);
+		logger.info("Login page is oppened");
 
 	}
 }
