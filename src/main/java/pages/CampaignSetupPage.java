db821ad19316c1e00118de10933268a7e9951a7f
AyaHamdyAhmed <46898899+AyaHamdyAhmed@users.noreply.github.com>
2019-09-29 17:18:40 +0200

    Update CampaignSetupPage.java

--- src/main/java/pages/CampaignSetupPage.java
+++ src/main/java/pages/CampaignSetupPage.java
@@ -1,19 +1,23 @@
 package pages;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class CampaignSetupPage extends PageBase {
 	WebElement elem;
+	private Logger logger = Logger.getLogger(CampaignSetupPage.class);
 
 	public CampaignSetupPage(WebDriver driver) {
 		this.driver = driver;
+		LogConfig();
 	}
 
 	public void SetUpCampaignData() {
 		Initialize();
 		initializeData();
 		SwitchToNewTab(driver);
+		logger.info("new tab is oppend");
 		InsertCampaignName(dataparser.getData("CamapaignName"));
 		InsertFundRaisingGoal(dataparser.getData("FundGoal"));
 		ClickNextStep();
@@ -22,15 +26,18 @@ public class CampaignSetupPage extends PageBase {
 
 	private void InsertCampaignName(String CName) {
 		SendkeysToElemnent(driver, parser.getbjectLocator("Campaignname"), CName);
+		logger.info("Camapign Name is Entered");
 	}
 
 	private void InsertFundRaisingGoal(String Fgoal) {
 		SendkeysToElemnent(driver, parser.getbjectLocator("FundraisingGoal"), Fgoal);
+		logger.info("Campaign Goal is Entered");
 	}
 
 	private void ClickNextStep() {
 		elem = driver.findElement(parser.getbjectLocator("NextButn"));
 		clickOnElementByJavaScript(driver, elem);
+		logger.info("Next Step button is clicked");
 	}
 
 	private void openThemePage() {
@@ -42,6 +49,7 @@ public class CampaignSetupPage extends PageBase {
 			waitForElementToBeInteractable(driver, parser.getbjectLocator("SkipBtn"));
 			clickOnElementByJavaScript(driver, elem);
 			LongWait();
+			logger.info("skip button is clicked");
 		}
 	}
 }
