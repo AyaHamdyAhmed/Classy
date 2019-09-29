da9a6687cb76de75e46ee03fd31c83c5d2ca33f5
AyaHamdyAhmed <46898899+AyaHamdyAhmed@users.noreply.github.com>
2019-09-29 17:18:34 +0200

    Update CampaignOverviewPage.java

--- src/main/java/pages/CampaignOverviewPage.java
+++ src/main/java/pages/CampaignOverviewPage.java
@@ -2,14 +2,16 @@ package pages;
 
 import static org.testng.Assert.assertEquals;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class CampaignOverviewPage extends PageBase {
 	WebElement elem;
-
+	private Logger logger = Logger.getLogger(PageBase.class);
 	public CampaignOverviewPage(WebDriver driver) {
 		this.driver = driver;
+		LogConfig();
 	}
 
 	public void publishCampaign() {
@@ -18,19 +20,21 @@ public class CampaignOverviewPage extends PageBase {
 		clickPublishFromPopup();
 		implicitWaitUntilLoadWithLongWait(driver);
 		LonglongWait();
-		// AssertThatCampaignisPublished();
 	}
 
 	private void clickPublishCamapaign() {
 		elem = driver.findElement(parser.getbjectLocator("PublishBtn"));
 		waitForElementToAppear(driver, parser.getbjectLocator("PublishBtn"));
 		clickOnElementByJavaScript(driver, elem);
+		logger.info("publish button is clicked");
 		implicitWaitUntilLoad(driver);
+        logger.info("puplish popup is appeared");
 	}
 
 	private void clickPublishFromPopup() {
 		elem = driver.findElement(parser.getbjectLocator("PublishPopup"));
 		clickOnElementByJavaScript(driver, elem);
+		logger.info("confirm publish campaign is clicked");
 	}
 
 	public void AssertThatCampaignisPublished() {
@@ -44,10 +48,6 @@ public class CampaignOverviewPage extends PageBase {
 		elem = driver.findElement(parser.getbjectLocator("PreViewMenu"));
 		assertEquals(elem.getText(), "VIEW");
 	}
-	/*
-	 * public void assertThatCamapignPublished(String url) {
-	 * assertEquals(driver.getCurrentUrl().replaceAll("[0-9]", ""), url); }
-	 */
 
 	public void AssertThatcampaignisnotpublished() {
 		Initialize();
