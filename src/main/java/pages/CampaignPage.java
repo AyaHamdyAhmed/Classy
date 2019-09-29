7d459e084cc10091c1966b311c9db360033b4c4d
AyaHamdyAhmed <46898899+AyaHamdyAhmed@users.noreply.github.com>
2019-09-29 17:18:37 +0200

    Update CampaignPage.java

--- src/main/java/pages/CampaignPage.java
+++ src/main/java/pages/CampaignPage.java
@@ -1,11 +1,14 @@
 package pages;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class CampaignPage extends PageBase {
+	private Logger logger = Logger.getLogger(CampaignPage.class);
 	public CampaignPage(WebDriver driver) {
 		this.driver = driver;
+		LogConfig();
 	}
 
 	public void createNewCampaign() {
@@ -18,12 +21,15 @@ public class CampaignPage extends PageBase {
 		implicitWaitUntilLoad(driver);
 		WebElement elem = driver.findElement(parser.getbjectLocator("CreateNew"));
 		clickOnElementByJavaScript(driver, elem);
+		logger.info("Create New drob down is oppened");
 	}
 
 	private void ClickOnDonationItem() {
 		implicitWaitUntilLoad(driver);
 		WebElement elem = driver.findElement(parser.getbjectLocator("DonationListItem"));
 		clickOnElementByJavaScript(driver, elem);
+		logger.info("donation item list is selected");
 		implicitWaitUntilLoadWithLongWait(driver);
+		logger.info("Camapign setup page is oppened");
 	}
 }
