769fea29b162212fe37191cfd1cf8adc73230433
AyaHamdyAhmed <46898899+AyaHamdyAhmed@users.noreply.github.com>
2019-09-29 17:18:49 +0200

    Update LoginPage.java

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,15 +1,17 @@
 package pages;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 
 public class LoginPage extends PageBase {
 //variables 
 	String UserName;
 	String Password;
-
+	private Logger logger = Logger.getLogger(LoginPage.class);
 // Keywords
 	public LoginPage(WebDriver driver) {
 		this.driver = driver;
+		LogConfig();
 	}
 
 	public void Login() {
@@ -23,15 +25,18 @@ public class LoginPage extends PageBase {
 
 	private void SetUserName(String username) {
 		driver.findElement(parser.getbjectLocator("UserName")).sendKeys(username);
+		logger.info("UserName Entered");
 	}
 
 	private void SetPassword(String Password) {
 		driver.findElement(parser.getbjectLocator("Password")).sendKeys(Password);
+		logger.info("Password Entered");
 	}
 
 	private void ClickLogin() {
 		ClickOnElement(driver, parser.getbjectLocator("SignIN"));
 		implicitWaitUntilLoad(driver);
+		logger.info("Login Clicked and Admin DashBoard Is oppened");
 	}
 
 }
